1e6afba7dfd52d8f4f7e4ac650605f380a7271ab
Matheus Figueiredo <matheus.figueiredo@otterwise.co>
2021-10-13 16:25:45 -0300

    Feat: Adiciona exercício para turma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from './routes/Dashboard'
+import Login from './routes/Login'
+import JokeDetails from './routes/JokeDetail'
 
-function App() {
+// Criar a navegação entre os componentes a seguir, utilizar a lib react-router-dom
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Dashboard />
+      <Login />
+      <JokeDetails />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/routes/Login.jsx
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      {/* O usuário deve ser redirecionado para tela de dashboard */}
+    </div>
+  )
+}
+
+export default Login
